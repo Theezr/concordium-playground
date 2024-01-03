53a8a5243054302a051c73aad6931da7efb384ea
Theezr <nickvanrossenberg@hotmail.com>
2024-01-03 13:53:39 +0100

    frontend

--- frontend/src/app/components/CcdClient.ts
+++ frontend/src/app/components/CcdClient.ts
@@ -1,14 +1,4 @@
-import { Buffer } from 'buffer/';
-
 import { detectConcordiumProvider, WalletApi } from '@concordium/browser-wallet-api-helpers';
-import { ModuleReference } from '@concordium/web-sdk';
-
-export type ContractName = 'project_token' | 'carbon_credits' | 'carbon_credit_market';
-export interface ContractInfo {
-  schemaBuffer: Buffer;
-  contractName: ContractName;
-  moduleRef?: ModuleReference.Type;
-}
 
 export async function connectToWallet(): Promise<{ provider: WalletApi; account: string }> {
   const provider = await detectConcordiumProvider();
